1f27c39eecb920858db4aab3e4275004dbf07f28
francescaguzzi <fguzzi99@gmail.com>
2025-11-23 03:00:20 +0100

    album gallery fix

--- quartz/components/pages/Content.tsx
+++ quartz/components/pages/Content.tsx
@@ -1,12 +1,26 @@
-import { ComponentChildren } from "preact"
 import { htmlToJsx } from "../../util/jsx"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "../types"
+// Importiamo la galleria salendo di un livello (..) perché siamo nella cartella 'pages'
+import AlbumGallery from "../AlbumGallery" 
 
-const Content: QuartzComponent = ({ fileData, tree }: QuartzComponentProps) => {
-  const content = htmlToJsx(fileData.filePath!, tree) as ComponentChildren
-  const classes: string[] = fileData.frontmatter?.cssclasses ?? []
+const Content: QuartzComponent = (props: QuartzComponentProps) => {
+  const { fileData, tree } = props
+  const content = htmlToJsx(fileData.filePath!, tree)
+  const classes = fileData.frontmatter?.cssclasses ?? []
   const classString = ["popover-hint", ...classes].join(" ")
-  return <article class={classString}>{content}</article>
+
+  // Prepariamo il componente
+  const Gallery = AlbumGallery
+
+  return (
+    <article class={classString}>
+      {/* 1. Prima il contenuto testuale della tua nota Obsidian */}
+      {content}
+      
+      {/* 2. Poi la griglia (che apparirà solo nella pagina giusta grazie al filtro interno) */}
+      <Gallery {...props} />
+    </article>
+  )
 }
 
 export default (() => Content) satisfies QuartzComponentConstructor
